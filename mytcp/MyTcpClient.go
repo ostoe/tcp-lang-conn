ee80a905abc9b10ce6727f1e8b9bf171c0b12cc0
linhaifly@163.com <linhaifly@163.com>
2022-04-26 15:48:24 +0800

    release v0.1

--- mytcp/MyTcpClient.go
+++ mytcp/MyTcpClient.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"syscall"
+
 	//"syscall"
 
 	"time"
 )
-
-
 
 func MyTcpClient(str string) {
 
@@ -37,7 +37,7 @@ func MyTcpClient(str string) {
 
 	startTime := time.Now()
 	fmt.Println("Client Time:", startTime.String())
-	//go circlePrint("Client: ", 1000)
+	go circlePrint("Client: ", 1000)
 	go func() {
 		var str string
 		for {
@@ -48,15 +48,20 @@ func MyTcpClient(str string) {
 	}()
 	for {
 		_, e := connCheck(conn)
-		if e == io.EOF {
+		if e == io.EOF || e == syscall.ECONNRESET {
 			endTime := time.Now()
-			fmt.Println("---------io.EOF-->END")
+			var connErrString = ""
+			if e == io.EOF {
+				connErrString = "EOF"
+			} else {
+				connErrString = "RST"
+			}
+			fmt.Printf("----------->END by [%s]\n", connErrString)
 			fmt.Println(endTime.String())
 			difference := endTime.Sub(startTime)
 			fmt.Printf("difference: %v\n", difference)
 			return
 		}
-
 		//_ = conn.SetReadDeadline(time.Now().Add(time.Millisecond * 1))
 		//content := make([]byte, 0);
 		//
@@ -70,8 +75,7 @@ func MyTcpClient(str string) {
 		//} else {
 		//	fmt.Println(string(content))
 		//}
-		time.Sleep(time.Second * 1)
-
+		time.Sleep(time.Millisecond * 10)
 
 		//interval := fmt.Sprintf("%d-", tag)
 		//conn.SetReadDeadline(time.Now())
@@ -93,7 +97,6 @@ func MyTcpClient(str string) {
 		//	conn.SetReadDeadline(time.Now().Add(10 * time.Millisecond))
 		//}
 
-
 		//fmt.Println(interval)
 		//file, err := os.OpenFile(logFileName, os.O_APPEND | os.O_WRONLY | os.O_CREATE, os.ModeAppend)
 		////_, err := io.WriteString(file, interval)
@@ -104,16 +107,13 @@ func MyTcpClient(str string) {
 
 }
 
-
 func circlePrint(name string, sleepTime int64) {
 	var printIndex = 0
-	var circleString = [4] string {"-", "\\", "|", "/"};
+	var circleString = [4]string{"-", "\\", "|", "/"}
 
 	for {
-		fmt.Printf("\r %s%c[%d;%dm%s%c[0;0m ", name,0x1B, 4,33, circleString[printIndex%4], 0x1B)
+		fmt.Printf("\r %s%c[%d;%dm%s%c[0;0m ", name, 0x1B, 4, 33, circleString[printIndex%4], 0x1B)
 		printIndex++
 		time.Sleep(time.Millisecond * time.Duration(sleepTime))
 	}
 }
-
-
--- mytcp/MyTcpServer.go
+++ mytcp/MyTcpServer.go
@@ -29,16 +29,16 @@ func MyTcpServer(str string) {
 	//}
 	for {
 		conn, err := listener.Accept()
-		if err != nil {continue}
+		if err != nil {
+			continue
+		}
 		go HandleConn(conn)
 	}
 }
 
-
-
 func HandleConn(conn net.Conn) {
-	if i,ok := conn.(*net.TCPConn); ok {
-		err := i.SetKeepAlive(false);
+	if i, ok := conn.(*net.TCPConn); ok {
+		err := i.SetKeepAlive(false)
 		CheckError(err)
 	}
 	defer conn.Close()
@@ -47,31 +47,34 @@ func HandleConn(conn net.Conn) {
 	conn.Write([]byte(daytime))
 	//tag :=	rand.Intn(100)
 
-	fmt.Println("New Tcp Connection:",conn.RemoteAddr())
+	fmt.Println("[New] Tcp Connection:", conn.RemoteAddr())
 	//remoteAddr := conn.RemoteAddr()
-	//go circlePrint("Server: ", 1000)
+	go circlePrint("Server: ", 1000)
 	for {
-		hasData , e := connCheck(conn)
+		hasData, e := connCheck(conn)
 		//fmt.Println("return error", e)
 		if e == io.EOF {
-			fmt.Println(conn.RemoteAddr(), "Closed")
+			fmt.Println("[FIN]", conn.RemoteAddr(), "Closed")
+			return
+		} else if e == syscall.ECONNRESET {
+			fmt.Printf("[RET] %s: %s\n", e, conn.RemoteAddr())
 			return
 		}
 		if hasData {
-		//	_ = conn.SetReadDeadline(time.Now().Add(time.Millisecond * 1005))
-			content := make([]byte, 1024);
+			//_ = conn.SetReadDeadline(time.Now().Add(time.Millisecond * 1005))
+			content := make([]byte, 1024)
 
-			_, TCPErr := conn.Read(content);
+			_, TCPErr := conn.Read(content)
 			if TCPErr != nil { // i/o timeout
-				fmt.Println("--",TCPErr)
+				//fmt.Println("--",TCPErr)
 				if TCPErr == io.EOF {
-					fmt.Println(conn.RemoteAddr(), "Closed")
+					fmt.Println("FIN", conn.RemoteAddr(), "Closed")
 					return
 				}
 			} else {
 				fmt.Println(string(content))
 			}
-			//conn.SetReadDeadline(time.Millisecond)
+			//conn.SetReadDeadline(time.Millisecond * 1)
 			//conn.SetReadDeadline(time.Time{}) // 取消设置
 		}
 		//file, err := os.OpenFile(logFileName, os.O_APPEND | os.O_WRONLY | os.O_CREATE, os.ModeAppend)
@@ -99,24 +102,26 @@ func HandleConn(conn net.Conn) {
 	}
 }
 
-
-func connCheck( conn net.Conn) (bool, error) {
+func connCheck(conn net.Conn) (bool, error) {
 	var sysErr error = nil
-	var hasData bool = false;
+	var hasData bool = false
 	rawConn, err := conn.(syscall.Conn).SyscallConn()
-	err = rawConn.Read(func(fd uintptr)  bool {
+	err = rawConn.Read(func(fd uintptr) bool {
 		var buf []byte = []byte{0}
 
 		//fmt.Println(len(buf), cap(buf))
 		// sub
 		//n, err := syscall.Read(int(fd), buf[:])
-		n, _, err := syscall.Recvfrom(int(fd), buf, syscall.MSG_PEEK | syscall.MSG_DONTWAIT )
+		n, _, err := syscall.Recvfrom(int(fd), buf, syscall.MSG_PEEK|syscall.MSG_DONTWAIT)
 		//fmt.Println("line:", err, n, buf)
-		switch  {
+		switch {
 		case n == 0 && err == nil:
 			sysErr = io.EOF
 		case err == syscall.EAGAIN || err == syscall.EWOULDBLOCK:
 			sysErr = nil
+		//ECONNRESET (54) syscall.Errno
+		//case err == syscall.ECONNRESET:connection reset by peer 收到reset报文
+		//	sysErr = syscall.ECONNRESET
 		case n == 1 && err == nil:
 			hasData = true
 		default:
@@ -125,11 +130,14 @@ func connCheck( conn net.Conn) (bool, error) {
 		return true
 	})
 	//fmt.Println("syscall raw error", err)
-	if err != nil {return hasData, err}
+	if err != nil {
+		return hasData, err
+	}
 	return hasData, sysErr
 }
 
-
 func CheckError(err error) {
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 }
