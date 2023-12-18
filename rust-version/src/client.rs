5b71301681e51f4811422226ada4e9822372a18e
linhaifly@163.com <linhaifly@163.com>
2023-12-18 19:06:34 +0800

    abcd

--- rust-version/src/client.rs
+++ rust-version/src/client.rs
@@ -48,13 +48,13 @@ pub fn start_client(addr_port: &str) {
     // 正常检测，只能检测linux链接自己的状态,和链路正常的状态
     thread::spawn(move || {
 
-        let mut stream1 = match TcpStream::connect(&addr_port_move) {
-            Ok(t) => t,
-            Err(e) => {
-                println!("[{}]: {}", "Connection failed", e.kind());
-                std::process::exit(1);
-            }
-        };
+        // let stream1 = match TcpStream::connect(&addr_port_move) {
+        //     Ok(t) => t,
+        //     Err(e) => {
+        //         println!("[{}]: {}", "Connection failed", e.kind());
+        //         std::process::exit(1);
+        //     }
+        // };
 
 
         // let mut stream = TcpStream::connect(addr_port_move).expect("connection failed!");
@@ -77,7 +77,7 @@ pub fn start_client(addr_port: &str) {
         }
         let start_time = std::time::Instant::now();
         loop {
-            stream1.set_write_timeout(None);
+            // stream1.set_write_timeout(None);
             thread::sleep(check_interval);
             // 一个一直空着的链接，
             let check_result = check_unit(&mut stream, start_time);
@@ -378,6 +378,7 @@ pub fn probe_timing_thread(
                                 }
                             } else if cfg!(target_os = "macos") {
                                 unsafe {
+                                    
                                     let a = setsockopt(
                                         stream.as_raw_fd(),
                                         0x06,
